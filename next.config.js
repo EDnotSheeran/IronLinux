3c18dbc92fb8e1cde481dff809363188819e8003
EDnotSheeran <juninho.tab3@gmail.com>
2021-08-25 21:36:09 -0300

    fix: arrumei os pacotes

--- next.config.js
+++ next.config.js
@@ -1,19 +1,12 @@
-const withImages = require('next-images')
-
 module.exports = {
-  ...withImages({
-    esModule: true
-  }),
-  ...{
-    reactStrictMode: true,
-    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-    images: {
-      domains: [
-        'tailwindui.com',
-        'images.unsplash.com',
-        'avatars.githubusercontent.com'
-      ]
-    }
-  }
-}
+  reactStrictMode: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  images: {
+    domains: [
+      'tailwindui.com',
+      'images.unsplash.com',
+      'avatars.githubusercontent.com',
+    ],
+  },
+};
