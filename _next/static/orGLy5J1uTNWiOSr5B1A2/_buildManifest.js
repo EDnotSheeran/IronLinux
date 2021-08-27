4252fb1666e7f600c77a520aebc0f0ea65f0ef6b
EDnotSheeran <EDnotSheeran@users.noreply.github.com>
2021-08-27 01:08:32 +0000

    Deploying to gh-pages from @ DevFigueiredo/IronLinuxFrontEnd@bfc1d638ded802679bb4f32e59f6fce9024668b0 🚀

--- _next/static/orGLy5J1uTNWiOSr5B1A2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/734-a6a00de2d5c0b86800f5.js","static/chunks/pages/index-00e47b95d51d8b05a1c9.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
