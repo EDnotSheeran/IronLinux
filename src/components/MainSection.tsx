9686f6da6097d327f584c21153e1c9a0df43ca5a
EDnotSheeran <juninho.tab3@gmail.com>
2021-08-26 22:00:22 -0300

    fix: toma daniel

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -4,24 +4,24 @@ const MainSection: React.FC = () => {
   return (
     <section className="flex justify-center items-center gap-10 mt-10 flex-col md:flex-row">
       <div className="max-w-xl px-5">
-        <h3 className="font-bold text-5xl font-poppins">
+        <h3 className="font-bold text-4xl font-poppins">
           Quer aprender mais sobre o mundo de{' '}
           <span className="text-gold">Linux</span> e{' '}
           <span className="text-gold">Segurança</span>?
         </h3>
-        <p className="font-poppins text-brown text-lg mt-6">
+        <p className="font-poppins text-brown text-base mt-6">
           IronLinux presta serviços de treinamento e consultoria nas principais
           áreas de segurança e sistemas operacionais. Descubra mais entrando em
           contato conosco.
         </p>
-        <button className="mt-8 border-2 rounded-lg p-3 font-bold border-black hover:bg-black hover:text-white">
+        <button className="mt-8 border-2 rounded-lg p-3 font-bold border-black hover:bg-black hover:text-white transition-colors">
           Conheça nossas Consultorias
         </button>
-        <button className="mt-8 bg-gold text-white border-gold font-bold border-2 rounded-lg py-3 px-6 ml-4 hover:bg-transparent hover:text-gold">
+        <button className="mt-8 bg-gold text-white border-gold font-bold border-2 rounded-lg py-3 px-6 ml-4 gold-button">
           Veja os Cursos
         </button>
       </div>
-      <div className="hidden md:flex items-center justify-center justify-items-center">
+      <div className=" md:flex items-center justify-center justify-items-center">
         <img
           className="w-full max-w-md"
           src="/people-studying-on-pc.svg"
